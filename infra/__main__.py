ccd219362186b64b5193cea1a8205dc8498dba52
JacksonMaxfield <jmaxfieldbrown@gmail.com>
2021-10-19 17:31:58 -0700

    Initial commit

---
+++ infra/__main__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from cdp_backend.infrastructure import CDPStack
+from pulumi import export
+
+###############################################################################
+
+cdp_stack = CDPStack(
+    gcp_project_id="cdp-self-chjtigrp",
+    firestore_location="us-central",
+)
+
+export("firestore_address", cdp_stack.firestore_app.app_id)
+export("gcp_bucket_name", cdp_stack.firestore_app.default_bucket)
